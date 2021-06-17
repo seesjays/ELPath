0dd180480c1e444fd1070aab57f3d21389e27f75
ChrisO <39662993+ChristianOhanaja@users.noreply.github.com>
2021-06-17 00:49:37 -0500

    testing dearpygui

---
+++ main.py
@@ -0,0 +1,12 @@
+from dearpygui import core, simple
+
+def save_callback(sender, data):
+    print("Save Clicked")
+
+with simple.window("Example Window"):
+    core.add_text("Hello world")
+    core.add_button("Save", callback=save_callback)
+    core.add_input_text("string")
+    core.add_slider_float("float")
+
+core.start_dearpygui()
